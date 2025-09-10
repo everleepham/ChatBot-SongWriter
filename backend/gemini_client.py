6d3f2011386b093c4ae4893f083e0cecd3be148b
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-09-10 17:33:19 +0200

    backend: connect with google ai

---
+++ backend/gemini_client.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.getenv("GENAI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+def generate_lyrics(prompt: str) -> str:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=2)
+        )
+    )
+    return response.text
