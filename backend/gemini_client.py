561406c0eab44b059faa833956f9943b2c3d8f5e
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-09-11 17:30:28 +0200

    backend: add endpoint for poem

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -8,12 +8,22 @@ api_key = os.getenv("GENAI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-def generate_lyrics(prompt: str) -> str:
+def songwriter(prompt: str) -> str:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt,
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=2)
+        )
+    )
+    return response.text
+
+def poemwriter(prompt: str) -> str:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=1)
         )
     )
     return response.text
